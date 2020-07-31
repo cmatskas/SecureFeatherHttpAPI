ba43b9ebb7020a29a37ecb9e36a0ba5ac7be2daf
Christos Matskas <christos.matskas@microsoft.com>
2020-07-31 16:25:17 +0000

    Added MS Graph call for downstream API

--- Program.cs
+++ Program.cs
@@ -1,27 +1,34 @@
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Graph;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.Resource;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
 
 namespace SecureFeatherHttpApi
 {
     class Program
-    {
+    { 
         static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddMicrosoftWebApiAuthentication(builder.Configuration);
+            var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+            builder.Services.AddMicrosoftWebApiAuthentication(builder.Configuration)
+                .AddMicrosoftWebApiCallsWebApi(builder.Configuration)
+                .AddInMemoryTokenCaches();
+
             builder.Services.AddAuthorization();
             var app = builder.Build();
-            app.UseRouting();
+            
             app.UseAuthentication();
             app.UseAuthorization();
             
-            app.MapGet("/api/todos", GetTodos);
-            app.MapPost("api/todos", CreateTodo);
+            app.MapGet("/api/todos", GetTodos).RequireAuthorization();
+            app.MapPost("api/todos", CreateTodo).RequireAuthorization();
+            app.MapGet("/api/me", GetGraphData).RequireAuthorization();
 
             await app.RunAsync();
         }
@@ -34,7 +41,7 @@ namespace SecureFeatherHttpApi
 
         static async Task GetTodos(HttpContext http)
         {
-            http.VerifyUserHasAnyAcceptedScope(new string[] {"test"});
+            http.VerifyUserHasAnyAcceptedScope(new string[] {"access_as_user"});
 
             var todos = new List<TodoItem> 
             {
@@ -42,6 +49,22 @@ namespace SecureFeatherHttpApi
                 new TodoItem{Id=2, Name="hello", IsComplete=true}
             };
             await http.Response.WriteJsonAsync(todos);
+        }
+
+        static async Task GetGraphData(HttpContext http)
+        {
+            http.VerifyUserHasAnyAcceptedScope(new string[]{"access_as_user"});
+
+            var tokenAcquisition = http.RequestServices.GetRequiredService<ITokenAcquisition>();
+
+            var authProvider = new DelegateAuthenticationProvider(async x => {
+                var accessToken = await tokenAcquisition.GetAccessTokenForUserAsync(new string[] {"User.Read"});
+                x.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            });
+            var graphClient = new GraphServiceClient(authProvider);
+            var me = await graphClient.Me.Request().GetAsync();
+
+            await http.Response.WriteJsonAsync(new {Name = me.GivenName, Email= me.Mail});
         }
     }
 }
